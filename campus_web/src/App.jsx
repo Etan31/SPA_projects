df94c09677591cc69af3371e6e5e852b4ab6265f
Tristan Ehron Tumbaga <tumbagatristanehron31@gmail.com>
2025-09-23 21:54:41 +0800

    New: completed dynamic headers on home, programs, and about-us section.

--- campus_web/src/App.jsx
+++ campus_web/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/TItle/Title'
+import About from './components/About/About'
+
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title = 'What We Offer'/>
+        <Programs />
+        <About />
+      </div>
     </div>
-  ) 
+  )
 }
 
 export default App
--- campus_web/src/components/Navbar/Navbar.jsx
+++ campus_web/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <nav>
-        <img src={logo} alt="logo" className='logo'/>
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li>Contact us</li>
-        </ul>
-    </nav>
-  )
+
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 500 ? setSticky(true) : setSticky(false);
+        })
+    }, []);
+
+
+    return (
+        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+            <img src={logo} alt="logo" className='logo' />
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About us</li>
+                <li>Campus</li>
+                <li>Testimonials</li>
+                <li>
+                    <button className='btn'>Contact us</button>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
